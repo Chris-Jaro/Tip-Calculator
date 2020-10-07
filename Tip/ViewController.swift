fc6518b9040ce65196378a6ec3d2df3d5f46e73a
Krzysztof Jarosz <jarosz.krzysztof001@gmail.com>
2020-10-07 15:04:19 +0200

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Chris Yarosh on 07/10/2020.
+//  Copyright © 2020 Chris Yarosh Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
